62958337dd838581c9614b057e47c0b06440078e
Timmy Hui <timmysone930@gmail.com>
2022-05-12 10:03:48 +0800

    Adjusted res record page

--- MobileApp/pages/ResRecordPage.tsx
+++ MobileApp/pages/ResRecordPage.tsx
@@ -31,8 +31,8 @@ export const ResRecordPage = (props: any) => {
             {/* {recordData.isSuccess && recordData.data.length !== 0 ? <ResRecordComponent data={recordData.data} props={props} refreshing={refreshing} onRefresh={onRefresh} /> :
                 <Text style={{ textAlign: 'center', fontSize: 17, margin: 20 }}>立刻去登記預約</Text>
             } */}
-            {recordData.isSuccess? recordData.data.length !== 0 && <ResRecordComponent data={recordData.data} props={props} refreshing={refreshing} onRefresh={onRefresh} /> :
-            <Text style={{ textAlign: 'center', fontSize: 17, margin: 20 }}>沒有預約記錄</Text>}
+            {recordData.isSuccess? recordData.data.length !== 0 ? <ResRecordComponent data={recordData.data} props={props} refreshing={refreshing} onRefresh={onRefresh} />:<Text style={{ textAlign: 'center', fontSize: 17, margin: 20 }}>沒有預約記錄</Text> 
+            : <Text style={{ textAlign: 'center', fontSize: 17, margin: 20 }}>沒有預約記錄</Text>}
         </SafeAreaView>
     )
 }
