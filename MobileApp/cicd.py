0cd61d23a97ee25bbefa8b9bd36045819ef5f852
reemo <r48n34@gmail.com>
2022-07-14 16:05:26 +0800

    Update cicd.py

--- MobileApp/cicd.py
+++ MobileApp/cicd.py
@@ -1,34 +1,39 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import argparse
 
+def mainFunc():
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
+    drive = GoogleDrive(gauth)
+
+    def ListFolder(parent):
+        filelist=[]
+        file_list = drive.ListFile({'q': "'%s' in parents and trashed=false" % parent}).GetList()
+        for f in file_list:
+            if f['mimeType']=='application/vnd.google-apps.folder': # if folder
+                filelist.append({"id":f['id'],"title":f['title'],"list":ListFolder(f['id'])})
+            else:
+                filelist.append({"title":f['title'],"title1":f['alternateLink']})
+        return filelist
+
+    # result = ListFolder('root')
+    # for i in result:
+    #     print(i)
+
+    # '1eiiATZdXHVSMJ5TV-AJEc-t604VlKeCz'
+
+    file1 = drive.CreateFile({
+        'title': 'telemedicine1.083.apk',
+        'parents': [{'id': '1eiiATZdXHVSMJ5TV-AJEc-t604VlKeCz'}]
+    })
+
+    file1.SetContentFile(apk_path)
+    file1.Upload() # Upload the file.
+    print('title: %s, id: %s' % (file1['title'], file1['id']))
+    
 apk_path = "./android/app/build/outputs/apk/release/app-release.apk"
 secret_path = "./client_secrets.json"
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
-drive = GoogleDrive(gauth)
-
-def ListFolder(parent):
-    filelist=[]
-    file_list = drive.ListFile({'q': "'%s' in parents and trashed=false" % parent}).GetList()
-    for f in file_list:
-        if f['mimeType']=='application/vnd.google-apps.folder': # if folder
-            filelist.append({"id":f['id'],"title":f['title'],"list":ListFolder(f['id'])})
-        else:
-            filelist.append({"title":f['title'],"title1":f['alternateLink']})
-    return filelist
-
-# result = ListFolder('root')
-# for i in result:
-#     print(i)
-
-# '1eiiATZdXHVSMJ5TV-AJEc-t604VlKeCz'
-
-file1 = drive.CreateFile({
-    'title': 'telemedicine1.083.apk',
-    'parents': [{'id': '1eiiATZdXHVSMJ5TV-AJEc-t604VlKeCz'}]
-})
-
-file1.SetContentFile(apk_path)
-file1.Upload() # Upload the file.
-print('title: %s, id: %s' % (file1['title'], file1['id']))
+if __name__ == "__main__":
+    mainFunc()
