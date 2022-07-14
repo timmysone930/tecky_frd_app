99090f55e19c7c4c3d04f439ce5514af5caf308f
reemo <r48n34@gmail.com>
2022-07-14 16:34:47 +0800

    Update cicd.py

--- MobileApp/cicd.py
+++ MobileApp/cicd.py
@@ -2,20 +2,14 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import argparse
 
-def mainFunc():
+
+
+def mainFunc(apps_name = "telemedicineApps.apk"):
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
     drive = GoogleDrive(gauth)
 
-    def ListFolder(parent):
-        filelist=[]
-        file_list = drive.ListFile({'q': "'%s' in parents and trashed=false" % parent}).GetList()
-        for f in file_list:
-            if f['mimeType']=='application/vnd.google-apps.folder': # if folder
-                filelist.append({"id":f['id'],"title":f['title'],"list":ListFolder(f['id'])})
-            else:
-                filelist.append({"title":f['title'],"title1":f['alternateLink']})
-        return filelist
+    
 
     # result = ListFolder('root')
     # for i in result:
@@ -24,7 +18,7 @@ def mainFunc():
     # '1eiiATZdXHVSMJ5TV-AJEc-t604VlKeCz'
 
     file1 = drive.CreateFile({
-        'title': 'telemedicine1.083.apk',
+        'title': apps_name,
         'parents': [{'id': '1eiiATZdXHVSMJ5TV-AJEc-t604VlKeCz'}]
     })
 
@@ -36,4 +30,16 @@ apk_path = "./android/app/build/outputs/apk/release/app-release.apk"
 secret_path = "./client_secrets.json"
 
 if __name__ == "__main__":
-    mainFunc()
+     # input args parser
+    parser = argparse.ArgumentParser(description='Process input mode.')
+    parser.add_argument('--name', default="telemedicineApps.apk", help='apps name')
+    args = parser.parse_args()
+
+    app_name = args.name
+    # print(args)
+
+    if args.name[-4:] != ".apk":
+        app_name += ".apk"
+
+    print(app_name)
+    mainFunc(app_name)
