1deb4ae1e93d6a287ac53fcb8aa17b8e63cd3a13
reemo <r48n34@gmail.com>
2022-08-31 10:16:31 +0800

    change py script code

--- MobileApp/incVersion.py
+++ MobileApp/incVersion.py
@@ -1,7 +1,9 @@
+# from asyncio.windows_events import NULL
 import re
 import os
+import sys
 
-def readVersion(gradle_path = "./android/app/build.gradle"):
+def readVersion(input_version = None, gradle_path = "./android/app/build.gradle"):
 
     f = open(gradle_path, "r")
     strs = f.read().splitlines()
@@ -15,6 +17,9 @@ def readVersion(gradle_path = "./android/app/build.gradle"):
             version_num = float(version_num_str) + float(0.01)
 
             new_version = str(round(version_num, 4))
+            if input_version != None:
+                new_version = input_version
+
             new_version_ref.append(new_version)
 
             print(f"Old version: {version_num_str}")
@@ -22,6 +27,8 @@ def readVersion(gradle_path = "./android/app/build.gradle"):
             
             strs[i] = v.replace(version_num_str, new_version)
             break
+    
+    print(f"versionName increased in {os.path.join(os.getcwd(), 'android', 'app', 'build.gradle' )}")
         
     f = open(gradle_path, "w")
     f.write("\n".join(strs))
@@ -40,16 +47,24 @@ def incEnvCode(env_path, code_version):
         strs = f.read().splitlines()
         f.close()
 
+        # Flag for checking if the regarding env exist
+        changed_version = False
+
         for i, v in enumerate(strs):
             if "VERSION_NUM=" in v:
 
                 strs[i] = "VERSION_NUM=" + str(code_version)
+                changed_version = True
                 break
+        
+        if not changed_version:
+            strs.append("VERSION_NUM=" + str(code_version))
         
         f = open(env_path, "w")
         f.write("\n".join(strs))
         f.close()
-        print(f"VERSION_NUM increased in {env_path}")
+        print(f"VERSION_NUM increased in {os.path.join(os.getcwd(),env_path)}")
     
 if __name__ == "__main__":
-    readVersion()
+    input_version = str(sys.argv[1]) if len(sys.argv) >= 2 else None
+    readVersion(input_version)
