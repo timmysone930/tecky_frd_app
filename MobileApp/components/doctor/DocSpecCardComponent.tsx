3f01900101457dea87ad1501de352d537ca87cbf
dennishooo <hkhdennis@gmail.com>
2023-02-09 15:43:13 +0800

    fix search bar

--- MobileApp/components/doctor/DocSpecCardComponent.tsx
+++ MobileApp/components/doctor/DocSpecCardComponent.tsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react'
-import { StyleSheet, Text, View, TouchableHighlight, Dimensions, ScrollView, RefreshControl } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import React, {useEffect} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Dimensions,
+  ScrollView,
+  RefreshControl,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { wait } from '../../pages/ResRecordPage';
-import { useGetAllSpecQuery } from '../../API/DoctorAPI';
-import { SpinnerComponent } from '../utils/SpinnerComponent';
+import {wait} from '../../pages/ResRecordPage';
+import {useGetAllSpecQuery} from '../../API/DoctorAPI';
+import {SpinnerComponent} from '../utils/SpinnerComponent';
 const windowHeight = Dimensions.get('window').height;
 // Doctor Type Name
 // const typeArr = ['所有專科', '未開放', '未開放', '未開放', '未開放', '未開放', '未開放', '未開放', '未開放',];
@@ -12,43 +20,48 @@ const windowHeight = Dimensions.get('window').height;
 // const typeIconArr = ['hospital-o', 'exclamation-circle', 'exclamation-circle', 'exclamation-circle', 'exclamation-circle', 'exclamation-circle', 'exclamation-circle', 'exclamation-circle', 'exclamation-circle']
 
 export const DocSpecCardComponent = (props: any) => {
-    // Card isPress Status
-    const [isPress, setIsPress] = React.useState(0);
+  // Card isPress Status
+  const [isPress, setIsPress] = React.useState(0);
 
-    // fetch data
-    const spec = useGetAllSpecQuery();
+  // fetch data
+  const spec = useGetAllSpecQuery();
 
-    // refetch
-    const [refreshing, setRefreshing] = React.useState(false);
-    const onRefresh = React.useCallback(() => {
-        try {
-            setRefreshing(true);
-            spec.refetch();
-            wait(2000).then(() => setRefreshing(false));
+  // refetch
+  const [refreshing, setRefreshing] = React.useState(false);
+  const onRefresh = React.useCallback(() => {
+    try {
+      setRefreshing(true);
+      spec.refetch();
+      wait(2000).then(() => setRefreshing(false));
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
 
-        } catch (e) {
-            console.log(e)
-        }
-    }, []);
+  return (
+    <>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={{backgroundColor: 'white'}}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        {/* <Text style={styles.indexTitle}>選擇專科服務類型</Text> */}
 
-    return (
-        <>
-            <ScrollView contentInsetAdjustmentBehavior="automatic" style={{ backgroundColor: 'white' }}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={onRefresh}
-                    />
-                }
-            >
-                {/* <Text style={styles.indexTitle}>選擇專科服務類型</Text> */}
+        {spec.isLoading && <SpinnerComponent />}
+        {spec.isError && (
+          <Text style={[styles.title, , styles.t_center]}>載入出現錯誤...</Text>
+        )}
 
-                {spec.isLoading && <SpinnerComponent />}
-                {spec.isError && <Text style={[styles.title, , styles.t_center]}>載入出現錯誤...</Text>}
-
-                {spec.isSuccess &&
-                    < View style={{ flexWrap: 'wrap', flexDirection: 'row', marginHorizontal: 15, marginBottom:20 }}>
-                        {/* <TouchableHighlight 
+        {spec.isSuccess && (
+          <View
+            style={{
+              flexWrap: 'wrap',
+              flexDirection: 'row',
+              marginHorizontal: 15,
+              marginBottom: 20,
+            }}>
+            {/* <TouchableHighlight 
                             key={`dr_type_${0}`} 
                             style={styles.drTypeCard} 
                             onPress={() => { props.props.navigation.navigate({ name: '醫生列表', params: { mode: '所有專科' } }) }} 
@@ -61,30 +74,41 @@ export const DocSpecCardComponent = (props: any) => {
                                 <Text style={isPress === (0 + 1) ? styles.drTypeTextAfterPressed : styles.drTypeTextBeforePressed}>所有專科</Text>
                             </>
                         </TouchableHighlight> */}
-                        {
-                            spec.data.map((item:any, idx:number) => (
-                                <TouchableHighlight 
-                                    key={`dr_type_${idx}`} 
-                                    style={styles.drTypeCard} 
-                                    onPress={ () => { 
-                                        props.props.navigation.navigate({ 
-                                            name: '醫生列表',
-                                            params: { mode: item }
-                                        }) 
-                                    }} 
-                                    activeOpacity={1} 
-                                    underlayColor="#6d7f99" 
-                                    onHideUnderlay={() => setIsPress(0)}
-                                    onShowUnderlay={() => { setIsPress(idx + 2) }}
-                                >
-                                    <>
-                                        <Icon name={'hospital-o'} size={30} color={isPress === (idx + 2) ? 'white' : 'grey'} style={{ marginBottom: 10 }} />
-                                        <Text style={isPress === (idx + 2) ? styles.drTypeTextAfterPressed : styles.drTypeTextBeforePressed}>{item}</Text>
-                                    </>
-                                </TouchableHighlight>
-                            ))
-                        }
-                        {/* {
+            {spec.data.map((item: any, idx: number) => (
+              <TouchableHighlight
+                key={`dr_type_${idx}`}
+                style={styles.drTypeCard}
+                onPress={() => {
+                  props.props.navigation.navigate({
+                    name: '醫生列表',
+                    params: {mode: item},
+                  });
+                }}
+                activeOpacity={1}
+                underlayColor="#6d7f99"
+                onHideUnderlay={() => setIsPress(0)}
+                onShowUnderlay={() => {
+                  setIsPress(idx + 2);
+                }}>
+                <>
+                  <Icon
+                    name={'hospital-o'}
+                    size={30}
+                    color={isPress === idx + 2 ? 'white' : 'grey'}
+                    style={{marginBottom: 10}}
+                  />
+                  <Text
+                    style={
+                      isPress === idx + 2
+                        ? styles.drTypeTextAfterPressed
+                        : styles.drTypeTextBeforePressed
+                    }>
+                    {item}
+                  </Text>
+                </>
+              </TouchableHighlight>
+            ))}
+            {/* {
                             typeArr.map((item, idx) => (
                                 <TouchableHighlight key={`dr_type_${idx}`} style={styles.drTypeCard} onPress={() => { props.props.navigation.navigate({ name: '醫生列表', params: { mode: item } }) }} activeOpacity={1} underlayColor="#6d7f99" onHideUnderlay={() => setIsPress(0)}
                                     onShowUnderlay={() => { setIsPress(idx + 1) }} disabled={typeStatusArr[idx]}>
@@ -95,59 +119,58 @@ export const DocSpecCardComponent = (props: any) => {
                                 </TouchableHighlight>
                             ))
                         } */}
-
-                    </View >
-                }
-            </ScrollView>
-        </>
-    )
-}
+          </View>
+        )}
+      </ScrollView>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-    // Title
-    indexTitle: {
-        marginTop: 22,
-        marginBottom: 5,
-        paddingHorizontal: 12,
-        fontSize: 16,
-        fontWeight: '600',
-        textDecorationLine: 'underline',
-        textAlign: 'center',
-    },
-    // card
-    drTypeCard: {
-        marginTop: 15,
-        marginLeft: 7,
-        backgroundColor: '#F4F4F4',
-        width: "31%",
-        height: windowHeight / 5,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 6,
-        shadowColor: Colors.shadow,
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.2,
-        shadowRadius: 2,
-    },
-    // Card text color
-    drTypeTextBeforePressed: {
-        fontWeight: '600',
-        color: 'grey',
-        fontSize: 18,
-        marginTop: 10,
-    },
-    drTypeTextAfterPressed: {
-        fontWeight: '600',
-        color: 'white',
-        fontSize: 18,
-        marginTop: 10,
-    },
-    title: {
-        fontSize: 16,
-        color: '#545454',
-        marginBottom: 8,
-    },
-    t_center: {
-        textAlign: 'center'
-    },
+  // Title
+  indexTitle: {
+    marginTop: 22,
+    marginBottom: 5,
+    paddingHorizontal: 12,
+    fontSize: 16,
+    fontWeight: '600',
+    textDecorationLine: 'underline',
+    textAlign: 'center',
+  },
+  // card
+  drTypeCard: {
+    marginTop: 15,
+    marginLeft: 7,
+    backgroundColor: '#F4F4F4',
+    width: '31%',
+    height: windowHeight / 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 6,
+    shadowColor: Colors.shadow,
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  // Card text color
+  drTypeTextBeforePressed: {
+    fontWeight: '600',
+    color: 'grey',
+    fontSize: 14,
+    marginTop: 10,
+  },
+  drTypeTextAfterPressed: {
+    fontWeight: '600',
+    color: 'white',
+    fontSize: 14,
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 16,
+    color: '#545454',
+    marginBottom: 8,
+  },
+  t_center: {
+    textAlign: 'center',
+  },
 });
--- MobileApp/pages/DocSpecPage.tsx
+++ MobileApp/pages/DocSpecPage.tsx
@@ -1,33 +1,59 @@
-import React from 'react'
-import { SafeAreaView, ScrollView } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import {Button} from 'native-base';
+import {color} from 'native-base/lib/typescript/theme/styled-system';
+import React from 'react';
+import {SafeAreaView, ScrollView} from 'react-native';
+import {Searchbar, Text} from 'react-native-paper';
 
 // import Config from 'react-native-config';
-import { DocSpecCardComponent } from '../components/doctor/DocSpecCardComponent';
+import {DocSpecCardComponent} from '../components/doctor/DocSpecCardComponent';
 
-export const DocSpecPage = (props:any) => {
-    // Set background to white
-    const backgroundStyle = { backgroundColor:'white' };
+export const DocSpecPage = (props: any) => {
+  // Set background to white
+  const backgroundStyle = {backgroundColor: 'white'};
 
-    return (
-        <SafeAreaView style={[backgroundStyle, { flex: 1 }]}>
-            {/* <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}> */}
-                {/* Card component */}
-                <Searchbar
-                    placeholder={"搜索醫生以及科目"}
-                    value={""}
-                    onPressIn={ () => {
-                        props.navigation.navigate({ 
-                            name: '醫生列表',
-                            params: { mode: '所有專科' }
-                        }) 
-                    }}
-                    inputStyle={{ fontSize: 16 }}
-                    iconColor={'#B7B7B7'}
-                    style={{ margin: 20, backgroundColor: '#F2F2F2', borderRadius: 13, shadowOpacity: 0.2, shadowRadius: 4, shadowOffset: { width: 0, height: 0 } }}
-                />
-                <DocSpecCardComponent props={props}/>
-            {/* </ScrollView> */}
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={[backgroundStyle, {flex: 1}]}>
+      {/* <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}> */}
+      {/* Card component */}
+      <Button
+        style={{
+          margin: 20,
+          backgroundColor: '#F2F2F2',
+          borderRadius: 13,
+          shadowOpacity: 0.2,
+          shadowRadius: 4,
+          shadowOffset: {width: 0, height: 0},
+        }}
+        onPress={() => {
+          props.navigation.navigate({
+            name: '醫生列表',
+            params: {mode: '所有專科'},
+          });
+        }}>
+        <Text style={{color: 'grey'}}>搜索醫生以及科目</Text>
+      </Button>
+      {/* <Searchbar
+        placeholder={'搜索醫生以及科目'}
+        value={''}
+        onPressIn={() => {
+          props.navigation.navigate({
+            name: '醫生列表',
+            params: {mode: '所有專科'},
+          });
+        }}
+        inputStyle={{fontSize: 16}}
+        iconColor={'#B7B7B7'}
+        style={{
+          margin: 20,
+          backgroundColor: '#F2F2F2',
+          borderRadius: 13,
+          shadowOpacity: 0.2,
+          shadowRadius: 4,
+          shadowOffset: {width: 0, height: 0},
+        }}
+      /> */}
+      <DocSpecCardComponent props={props} />
+      {/* </ScrollView> */}
+    </SafeAreaView>
+  );
+};
